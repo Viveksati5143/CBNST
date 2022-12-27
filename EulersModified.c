3680575563b2ba20ae77eb6786b24efb134bbb7c
VIVEK SATI <viveksati2002@gmail.com>
2022-12-28 04:08:51 +0530

    final commit

---
+++ EulersModified.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+float fun(float,float);
+int main(){
+    int i,j,c;
+    float x[100],y[100],h,m[100],m1,m2,a,s[100],w;
+    printf("\n     C program for Modified Euler Method \n\n");
+    printf("  Enter the initial value of x:");
+    scanf("%f",&x[0]);
+    printf("\n  Enter the value of increment h:");
+    scanf("%f",&h);
+    printf("\n  Enter the final value of x:");
+    scanf("%f",&a);
+    printf("\n  Enter the initial value of the variable y :");
+    scanf("%f",&y[0]);
+    s[0]=y[0];
+    for(i=1;x[i-1]<a;i++){
+        w=100.0;
+        x[i]= x[i-1]+h;
+        m[i]=fun(x[i-1],y[i-1]);
+        c=0;
+        while(w>0.0001){
+            m1=fun(x[i],s[c]);
+            m2=(m[i]+m1)/2;
+            s[c+1]=y[i-1]+m2*h;
+            w=s[c]-s[c+1];
+            w=fabs(w);
+            c=c+1;
+        }
+        y[i]=s[c];
+    }
+    printf("\n\n The respective values of x and y are\n     x  \t     y\n\n");
+    for(j=0;j<i;j++){
+        printf("  %f\t%f",x[j],y[j]);
+        printf("\n");
+    }
+}
+float fun(float a,float b){
+    float c;
+    c=a*a+b;
+    return(c);
+}
--- Eulers_Method.c
+++ Eulers_Method.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
-float f(float x,float y)
-{
-    return x+y;
-}
+#define f(x,y) x+y
+
 int main(){
-    float xx,h,x,y;
-    printf("\nEnter x0,y0,h,xn: ");
-    scanf("%f%f%f%f",&x,&y,&h,&xx);
-    int n=(xx-x)/h;
-    for(int i=0;i<n;i++){
-        y=y+(h*(f(x,y)));
-        x=x+h;
+    float x0, y0, xn, h, yn, slope;
+    int i, n;
+    printf("Enter Initial Condition\n");
+    printf("x0 = ");
+    scanf("%f", &x0);
+    printf("y0 = ");
+    scanf("%f", &y0);
+    printf("Enter calculation point xn = ");
+    scanf("%f", &xn);
+    printf("Enter number of steps: ");
+    scanf("%d", &n);
+    h = (xn-x0)/n;
+
+    printf("\nx0\ty0\tslope\tyn\n");
+    printf("------------------------------\n");
+    for(i=0; i < n; i++){
+    slope = f(x0, y0);
+    yn = y0 + h * slope;
+    printf("%.4f\t%.4f\t%0.4f\t%.4f\n",x0,y0,slope,yn);
+    y0 = yn;
+    x0 = x0+h;
     }
-    printf("%f\n",y);
+    printf("\nValue of y at x = %0.2f is %0.3f",xn, yn);
     return 0;
 }
