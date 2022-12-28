9a2f0a9ac9ecd1f87a4483dfa73ce66872477e7b
VIVEK SATI <viveksati2002@gmail.com>
2022-12-29 00:02:36 +0530

    final commit

--- Newton_Raphson.c
+++ Newton_Raphson.c
@@ -1,55 +1,45 @@
-//C Program to Implement Newton Raphson Method
 #include<stdio.h>
 #include<math.h>
 #define EPSILON 0.0001
 
-float f(float x)
-{
+float f(float x){
     return x*x*x - 3*x -5;
 }
-
-float differentiate(float x)
-{
+float differentiate(float x){
     return 3*x*x - 3;
 }
 
-int main()
-{
+int main(){
     int maxIteration,i;
     float x1,x2,x,x0;
     printf("Enter Maximum no of Iterations\n");
     scanf("%d",&maxIteration);
-
-//......Compute x1 and x2............. 
-   
-   do
-   {
+    
+    do{
         printf("Enter the value of x1 and x2(starting boundary)");
         scanf("%f%f",&x1,&x2);
-        if(f(x1)*f(x2)>0)
-        {
+        if(f(x1)*f(x2)>0){
            printf("Boundary Values are Invalid\n");
            continue;
         }
-        else
-        {
+        else{
             printf("Roots Lie between %f and %f\n",x1,x2);
             break;
         }
-    } while(1);
+    }while(1);
 
     if(fabs(f(x1)) < fabs(f(x2))) x0 = x1;
     else x1 = x2;    
 
     for(i=1;i<=maxIteration;i++){
-      x = x0 - (f(x0)/differentiate(x0));  
-      
-      if(fabs(x-x0)<EPSILON){  
-        printf("Iterations=%d  Final Root=%f\n",i,x);
-        return 0;
-      }      
-      printf("Iterations=%d  Roots=%f\n",i,x); 
-      x0=x;
+        x = x0 - (f(x0)/differentiate(x0));  
+
+        if(fabs(x-x0)<EPSILON){  
+            printf("Iterations=%d  Final Root=%f\n",i,x);
+            return 0;
+        }
+        printf("Iterations=%d  Roots=%f\n",i,x); 
+        x0=x;
     }
     printf("Root=%f  Total Iterations=%d",x,--i);
     return 0;
--- Regula_Falsi_Method.c
+++ Regula_Falsi_Method.c
@@ -3,47 +3,38 @@
 #define EPSILON 0.0001
 #define findValueAt(x) x*x*x - 2*x -5
 
-float findX(float x1,float x2)
-{
+float findX(float x1,float x2){
     return (x1*findValueAt(x2) - x2 * findValueAt(x1)) / (findValueAt(x2) - findValueAt(x1));
 }
-int main()
-{
+int main(){
     int maxIteration,i=1;
     float x1,x2,x3,x;
     printf("Enter Maximum no of Iterations\n");
     scanf("%d",&maxIteration);
-    
-   do
-   {
+
+    do{
         printf("Enter the value of x1 and x2(starting boundary->Initial Roots)");
         scanf("%f%f",&x1,&x2);
-        if(findValueAt(x1)*findValueAt(x2)>0)
-        {
+        if(findValueAt(x1)*findValueAt(x2)>0){
            printf("Roots are Invalid\n");
            continue;
         }
-        else
-        {
+        else{
             printf("Roots Lie between %f and %f\n",x1,x2);
             break;
-
         }
     } while(1);
-
     x = findX(x1,x2);   
-
-    do
-    {
-      if(findValueAt(x)*findValueAt(x1)<0) x2=x;    
-      else x1=x;
-      printf("Iterations=%d  Root=%f\n",i,x);   
-      x3 = findX(x1,x2);  
-      if(fabs(x3-x)<EPSILON){ 
-        printf("Root=%f  Total Iterations=%d",x,i);
-        return 0;
+    do{
+        if(findValueAt(x)*findValueAt(x1)<0) x2=x;    
+        else x1=x;
+        printf("Iterations=%d  Root=%f\n",i,x);   
+        x3 = findX(x1,x2);  
+        if(fabs(x3-x)<EPSILON){ 
+            printf("Root=%f  Total Iterations=%d",x,i);
+            return 0;
         }
-        x=x3;  //Important
+        x=x3;  
         i++; 
     }while(i<=maxIteration);
     printf("Root=%f  Total Iterations=%d",x,--i);
